575245ec25aeb2f6c36517df77896cdff9094a78
terkoizmy <dimas.aryan021@gmail.com>
2024-08-17 21:34:29 +0700

    post create page

--- src/app/(dashboard)/(routes)/post/create/page.tsx
+++ src/app/(dashboard)/(routes)/post/create/page.tsx
@@ -1,9 +1,53 @@
 "use client";
+import dynamic from 'next/dynamic';
+import { Button } from '@/components/ui/button';
+import MarkdownIt from 'markdown-it';
+import 'react-markdown-editor-lite/lib/index.css';
+import { useState, useCallback } from 'react';
+import { Input } from '@/components/ui/input';
+import { Label } from "@/components/ui/label";
 
-export default function CreatePost()  {
+
+const MdEditor = dynamic(() => import('react-markdown-editor-lite'), {
+  ssr: false,
+});
+
+// Initialize markdown parser outside the component
+const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+export default function CreatePost() {
+  const [content, setContent] = useState({ text: '', html: '' });
+
+  // Use useCallback to memoize the handleEditorChange function
+  const handleEditorChange = useCallback(({ html, text }: any) => {
+    setContent({ html, text });
+  }, []);
+
+  function save() {
+    console.log('Saving content:', content);
+    // Implement your save logic here
+  }
+
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      Ini page Create POST
-    </main>
+    <div>
+      <div>
+        <Label htmlFor="picture">Thumbnail</Label>
+        <Input id="picture" type="file" />
+      </div>
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="title">Title Post</Label>
+        <Input type="email" id="email" placeholder="e.g. 'Japan Lifts Megathrust Earth...' " />
+      </div>
+      <MdEditor 
+        style={{ height: '500px' }} 
+        renderHTML={text => mdParser.render(text)} 
+        onChange={handleEditorChange} 
+      />
+      <div className='flex justify-end'>
+        <Button onClick={save}>
+          Post
+        </Button>
+      </div>
+    </div>
   );
 }
--- src/app/(dashboard)/_components/navbar.tsx
+++ src/app/(dashboard)/_components/navbar.tsx
@@ -1,14 +1,12 @@
 "use client"
 
 import { NavbarRoutes } from "@/components/navbar-routes"
-
+import { MobileSidebar } from "./mobile-sidebar"
 
 export const Navbar = () => {
   return (
     <div className="p-4 border-b h-full flex items-center shadow-sm justify-end">
-      
-      {/* Buat Resposive Mobilenya nanti */}
-      {/* <MobileSidebar /> */}
+      <MobileSidebar />
       <NavbarRoutes />
     </div>
   )
